8ad41d523c99e92e7c95f24d8665a0ff5160ea33
Yuri Avdijevski <dtoxvanilla@gmail.com>
2023-08-25 15:35:42 +0100

    chore: implemented Bingo row logic
    added super basic UI to find out the outcome

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,22 @@ interface Data {
 }
 export default function Home() {
   // Your task is to write a function that determines whether this card will ever get Bingo.
+  let win: boolean = false;
+
   const winOrDie: boolean = useMemo((): boolean => {
-    let win: boolean = false;
     if (data.cards.length === 0) {
       return win;
     }
 
-    data.cards.forEach((card: number[]) => {
-      for (let i: number = 0; i < card.length - 1; i + 5) {
-        const cardRow = data.cards.slice(i, i + 5);
-        console.log(cardRow);
+    data.cards.forEach((card: number[], index: number) => {
+      //rows
+      for (let i: number = 0; i < card.length - 1; i += 5) {
+        const cardRow: number[] = card.slice(i, i + 5);
+        data.random.toString().includes(cardRow.toString())
+          ? (win = true)
+          : (win = false);
+        // console.log(`card ${index}`, cardRow);
+        //columns to do
       }
     });
 
@@ -26,7 +32,16 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">Welcome to Bingo Game</h1>
+      <section>
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to Bingo Game
+        </h1>
+        <p className="text-center text-xl mt-4">
+          {win
+            ? "We have survived"
+            : "Squid got us killed and our mission failed. Try again!"}
+        </p>
+      </section>
     </main>
   );
 }
