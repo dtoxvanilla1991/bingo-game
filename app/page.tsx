c6461879d61b20333fcb6027f8b8cdaa5c3a10dd
Yuri Avdijevski <dtoxvanilla@gmail.com>
2023-08-28 21:20:54 +0100

    chore: adding final logic for finding Bingo
    looping provided numbers in data object as supposed "random" numbers until we find Bingo or we "die" to squid

--- app/page.tsx
+++ app/page.tsx
@@ -12,23 +12,27 @@ interface CardProps {
 }
 export default function Home() {
   // Your task is to write a function that determines whether this card will ever get Bingo.
+  // Can you write a program that tells you which board to pick to guarantee a win against the giant squid?
   let win: boolean = false;
+  // destructuring the data
+  const { random, cards }: Data = data;
 
   const winOrDie: boolean = useMemo((): boolean => {
-    if (data.cards.length === 0) {
+    if (cards.length === 0) {
       return win;
     }
-    //create empty array of objects equal to the number of cards
-    //each object will have 2 key for rows and columns
-    //rows and columns will be Maps of numbers for easy retrieval and deletion
-    const cardsOnBoard: CardProps[] = data.cards.map(
+    // create empty array of objects equal to the number of cards
+    // each object will have 2 key for rows and columns
+    // rows and columns will be Maps of numbers for easy retrieval and deletion
+    const cardsOnBoard: CardProps[] = cards.map(
       (): CardProps => ({
         rows: new Map(),
         columns: new Map(),
       }),
     );
-    //we will loop through each card and check if the number is in the row or column and if it is, we will delete it
-    data.cards.forEach((card: number[], index: number) => {
+    // we will loop through each card and check if the number is in the row or column
+    // if it is, we will delete it
+    cards.forEach((card: number[], index: number): void => {
       //rows
       let rowNumber: number = 1;
       for (let i: number = 0; i < card.length; i += 5) {
@@ -49,12 +53,42 @@ export default function Home() {
         columnNumber++;
       }
     });
-    console.log(cardsOnBoard[0].columns);
-    //if the row or column is empty, we will set the win to true and break the loop
-
+    // if the row or column is empty, we will set the win to true and break the loop
+    if (random.length === 0) {
+      //to do
+      //generate random numbers until someone wins
+    }
+    random.forEach((n: number): void => {
+      cardsOnBoard.forEach((card: CardProps): void => {
+        //we check rows
+        card.rows.forEach((row: number[], key: number): void => {
+          if (win) return;
+          //    console.log("row", row);
+          if (row.includes(n)) {
+            row.splice(row.indexOf(n), 1);
+            if (row.length === 0) {
+              win = true;
+              return;
+            }
+          }
+        });
+        //we check columns
+        card.columns.forEach((column: number[], key: number): void => {
+          if (win) return;
+          // console.log("column", column);
+          if (column.indexOf(n) !== -1) {
+            column.splice(column.indexOf(n), 1);
+            if (column.length === 0) {
+              win = true;
+              return;
+            }
+          }
+        });
+      });
+    });
+    console.log("WE HAVE A WINNER");
     return win;
   }, [data]);
-  // Can you write a program that tells you which board to pick to guarantee a win against the giant squid?
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section>
