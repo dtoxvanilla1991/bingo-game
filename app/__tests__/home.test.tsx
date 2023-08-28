c58edf99e3782da6e80d93b50d241bd16ac2a71c
Yuri Avdijevski <dtoxvanilla@gmail.com>
2023-08-29 00:06:52 +0100

    chore: added one last edge case to tests
    added 1 edge case test to main logic function of the app

--- app/__tests__/home.test.tsx
+++ app/__tests__/home.test.tsx
@@ -1,10 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe } from "node:test";
+import data from "../mockedData/dataForTests.json";
 import Home from "@/app/page";
 
 describe("home page", () => {
   it("renders homepage unchanged", () => {
     const { container } = render(<Home />);
     expect(container).toMatchSnapshot();
+  });
+  it("squid wins, squad loses Bingo and dies if there are no Cards", () => {
+    // added dataForTests here to imitate empty cards array
+    render(<Home />);
+    setTimeout(() => {
+      const squidWins = screen.getByText(
+        "Squid got us killed and our mission failed. Try again!",
+      );
+      expect(squidWins).toBeInTheDocument();
+    }, 2500);
   });
 });
