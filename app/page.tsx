cf44c9ac92a50c757535c6945f528fb1c0bf07b6
Yuri Avdijevski <dtoxvanilla@gmail.com>
2023-08-28 17:53:03 +0100

    chore: changing the approach of the game solution
    should be better approach to solve as it will support dynamic random number generation later, and right now we will be able to find numbers matches in any row or column

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ interface Data {
   random: number[];
   cards: number[][] | number[];
 }
+
+interface CardProps {
+  rows: Map<number, number[]>;
+  columns: Map<number, number[]>;
+}
 export default function Home() {
   // Your task is to write a function that determines whether this card will ever get Bingo.
   let win: boolean = false;
@@ -13,23 +18,28 @@ export default function Home() {
     if (data.cards.length === 0) {
       return win;
     }
-
+    //create empty array of objects equal to the number of cards
+    //each object will have 2 key for rows and columns
+    //rows and columns will be Maps of numbers for easy retrieval and deletion
+    const cardsOnBoard: CardProps[] = data.cards.map(() => ({
+      rows: new Map(),
+      columns: new Map(),
+    }));
+    //we will loop through each card and check if the number is in the row or column and if it is, we will delete it
     data.cards.forEach((card: number[], index: number) => {
-      //rows
-      for (let i: number = 0; i < card.length - 1; i += 5) {
+      let rowNumber: number = 1;
+      for (let i: number = 0; i < card.length; i += 5) {
         const cardRow: number[] = card.slice(i, i + 5);
-        data.random.toString().includes(cardRow.toString())
-          ? (win = true)
-          : (win = false);
-        // console.log(`card ${index}`, cardRow);
-        //columns to do
+        cardsOnBoard[index].rows.set(rowNumber, cardRow);
+        rowNumber++;
       }
     });
+    console.log(cardsOnBoard[1].rows);
+    //if the row or column is empty, we will set the win to true and break the loop
 
     return win;
   }, [data]);
   // Can you write a program that tells you which board to pick to guarantee a win against the giant squid?
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section>
